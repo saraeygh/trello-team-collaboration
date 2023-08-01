d76fb1b14adc5b009f46a8b584fa22c3f10b2e34
unknown <you@example.com>
2023-08-01 22:33:43 +0330

    project model and admin

--- workspace/admin.py
+++ workspace/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+from workspace import models
 
 
 # Mahdieh
+@admin.register(models.Workspace)
 class Workspace(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'description', 'is_admin']
+    list_editable = ['description', 'name']
+    list_filter = ['name']
+    list_per_page = 10
 
+    @admin.display(ordering='access_level')
+    def is_admin(self, project):
+        if Workspace.access_level == 2:
+            return 'admin'
+        return 'member'
 
 # Mahdieh
-class BoardAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'members', 'color', 'owner']
+    list_editable = ['member', 'name', 'color']
+    list_filter = ['name', 'owner']
+    list_per_page = 10
+
--- workspace/models.py
+++ workspace/models.py
@@ -1,4 +1,4 @@
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from accounts.models import User
@@ -7,50 +7,88 @@ from core.models import TimeMixin
 
 # Mahdieh
 class Workspace(models.Model, TimeMixin):
+    class Access(models.IntegerChoices):
+        MEMBER = 1  # Can view and create and move only own items
+        ADMIN = 2  # Can remove members and modify project settings.
+
     name = models.CharField(
         max_length=255,
+        unique=True,
         blank=False,
         null=False,
+        help_text='Enter the name of the workspace.'
     )
     description = models.TextField(
         blank=True,
-        null=True
+        null=True,
+        help_text='Enter a description for the workspace'
     )
-    user = models.ForeignKey(
+    user = models.ManyToManyField(
         User,
-        on_delete=models.PROTECT
+        related_name='workspaces',
+        help_text="Users who are members of this workspace.",
+        on_delete=models.CASCADE
+    )
+    access_level = models.IntegerField(
+        choices=Access.choices,
+        default=1
     )
 
 
-#Mahdieh
-class Board(models.Model):
+# Mahdieh
+class Project(models.Model):
+
     owner_model = models.ForeignKey(
         ContentType,
         blank=False,
         null=False,
         related_name='board',
         on_delete=models.CASCADE,
-        limit_choices_to=models.Q(app_label='users', model='user') | models.Q(app_label='projects', model='project')
     )
     owner = GenericForeignKey(
         'owner_model',
         'owner_id'
     )
-    title = models.CharField(
+    name = models.CharField(
         max_length=255,
         blank=False,
-        null=False
+        null=False,
+        help_text='Enter the name of the project'
     )
     description = models.TextField(
         blank=True,
-        null=True)
+        null=True,
+        help_text="Enter a description for the project."
+    )
     # image = models.ImageField(
     #     blank=True,
     #     null=True,
-    #     upload_to='board_images'
+    #     upload_to='board_images',
+    #     help_text='Upload image to project'
     # )
     color = models.CharField(
         blank=True,
         null=False,
-        max_length=6
+        max_length=6,
+        help_text='Choose color to your project.'
     )
+    workspace = models.ForeignKey(
+        Workspace,
+        on_delete=models.CASCADE,
+        related_name='project',
+        help_text="Select the workspace this project belongs to."
+    )
+    members = models.ManyToManyField(
+        User,
+        # through='Workspace',
+        # through_fields=('project', 'member')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['owner_model', 'owner_id']),
+        ]
+
