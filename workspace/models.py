3908b79ffb808f2fbcf9e23cd26ca7f7cbd848b6
unknown <you@example.com>
2023-08-06 19:50:03 +0330

    fix:define new class of workspace

--- workspace/models.py
+++ workspace/models.py
@@ -7,80 +7,76 @@ from core.models import BaseModel, TimeMixin
 
 # Mahdieh
 class Workspace(TimeMixin, BaseModel):
-
-    class Access(models.IntegerChoices):
-        MEMBER = 1  # Can view and create and move only own items
-        ADMIN = 2  # Can remove members and modify project settings.
-
-    name = models.CharField(
+     name = models.CharField(
         max_length=255,
         blank=False,
         null=False,
-        help_text='Enter the name of the workspace.'
+        help_text=_('Enter the name of the workspace.')
     )
+     
+     def __str__(self):
+        return self.name
+     
+
+
+# Mahdieh
+class WorkspaceMember(TimeMixin, BaseModel):
+
+    class Access(models.IntegerChoices):
+        MEMBER = 1  # Can view and move only own items
+        ADMIN = 2  # Can  add and remove members and modify project settings.
+
+    workspace =models.ForeignKey(
+        Workspace,
+        on_delete=models.CASCADE,
+        verbose_name=_('Workspace'),
+        related_name='Workspace'
+    )
+
     description = models.TextField(
         blank=True,
         null=True,
-        help_text='Enter a description for the workspace'
+        help_text=_('Enter a description for the workspace')
     )
-    members = models.ManyToManyField(
+    members = models.ForeignKey(
         User,
         related_name='workspaces',
-        help_text="Users who are members of this workspace.",
+        on_delete=models.CASCADE,
+        help_text=_("Users who are members of this workspace."),
     )
     access_level = models.IntegerField(
         choices=Access.choices,
         default=1
     )
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['name']
-
-    def is_admin(self, user):
-        """Check if a given user is an admin in the workspace."""
-        return self.members.through.objects.filter(user=user, workspace=self, access_level=Workspace.Access.ADMIN).exists()
-
-    def add_member(self, user, access_level=Access.MEMBER):
-        """Add a user as a member to the workspace with the specified access level."""
-        if self.is_admin(user):
-            membership, created = self.members.through.objects.get_or_create(user=user, workspace=self)
-            membership.access_level = access_level
-            membership.save()
-
-    def remove_member(self, request, user):
-        """Remove a user from the workspace's members if the requester is an admin."""
-        if self.is_admin(request):
-            self.members.remove(pk=user)
-
-    def create_project(self, name, description=None):
-        """Create a new project within the workspace if the requester is an admin."""
-        return Project.objects.create(
-            name=self.name,
-            description=self.description,
-            workspace=self)
-
-    def delete_project(self, request, project):
-        """Delete a project within the workspace if the requester is an admin."""
-        if self.is_admin(request):
-            project.delete()
-
 
 # Mahdieh
 class Project(TimeMixin, BaseModel):
-
     name = models.CharField(
-        max_length=255,
+        max_length=200,
         blank=False,
         null=False,
-        help_text='Enter the name of the project'
+        help_text=_('Enter the name of the project')
     )
+
+    def __str__(self):
+        return self.name
+
+
+# Mahdieh
+class ProjectMember(TimeMixin, BaseModel):
+
+    Project = models.ForeignKey(
+        Project,
+        verbose_name=_('Project'),
+        related_name='Project',
+        on_delete=models.CASCADE,
+    )
+
     description = models.TextField(
         blank=True,
         null=True,
-        help_text="Enter a description for the project."
+        help_text=_("Enter a description for the project.")
     )
 
     # image = models.ImageField(
@@ -90,36 +86,22 @@ class Project(TimeMixin, BaseModel):
     #     help_text='Upload image to project'
     # )
 
+    members = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE,
+    help_text=_("Users who are members of this workspace."),
+    )
+
     workspace = models.ForeignKey(
         Workspace,
         on_delete=models.CASCADE,
         related_name='project',
-        help_text="Select the workspace this project belongs to."
+        help_text=_("Select the workspace this project belongs to.")
     )
 
     def __str__(self):
         return self.name
 
-    def get_lists(self):
-        """Get all lists associated with the project."""
-        return self.lists.all()
-
-    def create_card(self, title, description=None):
-        """Create a new task in the project.
-        """
-        default_list = self.get_lists().first()
-        return Task.objects.create(
-            title=title,
-            description=description,
-            list=default_list
-        )
-
-    def list_project(self, request, user):
-        """
-        Display list of all project
-        """
-        if request.user == user:
-            return Project.objects.all(pk=user)
 
 # Hossein
 class Task(TimeMixin, BaseModel):
@@ -140,7 +122,7 @@ class Task(TimeMixin, BaseModel):
     title = models.CharField(
         verbose_name=_("Title"),
         max_length=250,
-        help_text="Enter the title of the task."
+        help_text=_("Enter the title of the task.")
         )
 
     description = models.TextField(
@@ -226,17 +208,30 @@ class Assignment(TimeMixin, models.Model):
 
 
 # Reza
-class Label(TimeMixin, BaseModel):
+class Label(TimeMixin):
     name = models.CharField(
         verbose_name=_("Label name"),
         help_text=_("Insert label name"),
         max_length=50
         )
 
-    task = models.ManyToManyField(
-        "workspace.task",
-        verbose_name=_("Task"),
-        help_text="Task(s) with this label"
+    def __str__(self):
+        return self.name
+
+
+# Reza
+class LabeledTask(TimeMixin):
+    label = models.ForeignKey(
+        Label,
+        verbose_name=_("Label"),
+        help_text=_("Label to use"),
+        on_delete=models.CASCADE
+        )
+
+    task = models.ForeignKey(
+        Task,
+        verbose_name=_("Task to label"),
+        on_delete=models.CASCADE
         )
 
 
@@ -248,7 +243,7 @@ class Comment(TimeMixin, BaseModel):
         )
 
     user = models.ForeignKey(
-        "accounts.user",
+        User,
         verbose_name=_("user"),
         on_delete=models.CASCADE
         )
@@ -258,3 +253,6 @@ class Comment(TimeMixin, BaseModel):
         verbose_name=_("Task"),
         on_delete=models.CASCADE
         )
+
+    def __str__(self):
+        return self.text
