7c3ac17d58e31ecd832d9aba7b9ef675b9f61916
unknown <you@example.com>
2023-08-14 19:31:27 +0330

    fix: change m2m fields of workspace

--- workspace/models.py
+++ workspace/models.py
@@ -6,7 +6,13 @@ from django.utils import timezone
 
 
 # Mahdieh
-class Workspace(TimeMixin, models.Model):
+class Workspace(TimeMixin):
+
+    class Access(models.IntegerChoices):
+        MEMBER = 1  # Can view and move only own items
+        ADMIN = 2  # Can  add and remove members and modify project settings.
+    id = models.IntegerField(primary_key=True)
+    
     name = models.CharField(
         max_length=255,
         verbose_name=_("Workspace name"),
@@ -14,31 +20,13 @@ class Workspace(TimeMixin, models.Model):
         )
 
     description = models.TextField(
-        blank=True, null=True,
+        blank=True, 
+        null=True,
         help_text=_('Enter a description for the workspace')
         )
-
-    def __str__(self):
-        return self.name
-
-
-# Mahdieh
-class WorkspaceMember(TimeMixin, models.Model):
-
-    class Access(models.IntegerChoices):
-        MEMBER = 1  # Can view and move only own items
-        ADMIN = 2  # Can  add and remove members and modify project settings.
-
-    workspace = models.ForeignKey(
-        Workspace,
-        on_delete=models.CASCADE,
-        verbose_name=_('Workspace'),
-        related_name='workspace',
-        )
-
+    
     member = models.ForeignKey(
         User,
-        related_name='member',
         on_delete=models.CASCADE,
         help_text=_("Users who are members of this workspace."),
         )
@@ -48,20 +36,10 @@ class WorkspaceMember(TimeMixin, models.Model):
         default=1
         )
 
-    def add_member(self):
-        """
-        add member to group
-        """
-        self.access_level = 1
-        self.save()
+    def __str__(self):
+        return f'{self.member.first_name}, {self.member.last_name} , {self.project.name}'
 
-    def remove_member(self):
-        """
-        remove a members from a group
-        """
-        self.delete()
-
-
+        
 # Mahdieh
 class Project(TimeMixin, BaseModel):
     name = models.CharField(
@@ -89,13 +67,18 @@ class Project(TimeMixin, BaseModel):
         auto_now_add=True,
         help_text="The date and time when the project deadline."
         )
-    
+
+    member = models.ManyToManyField(
+        User,
+        through='ProjectMember', 
+        through_fields=('project', 'member')
+        )
+
     #slug = models.SlugField(
     #    max_length=50,
-    #    default=11
+    #    unique=True
     #    )
         
-    
     def __str__(self):
         return self.name
 
