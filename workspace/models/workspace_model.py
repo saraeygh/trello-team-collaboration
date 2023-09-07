8d86a4e89578c6ce1eb13db50553b3eb4b7896fc
MahdiehSoleimani <mahdieh.soleimani2000@gmail.com>
2023-09-07 16:39:35 +0330

    feat: add image of workspace

--- workspace/models/workspace_model.py
+++ workspace/models/workspace_model.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from workspace.validatores import validate_file_size
 from core.models import TimeMixin, BaseModel
 from accounts.models import User
 
@@ -24,6 +25,17 @@ class Workspace(TimeMixin, BaseModel):
         User,
         through='WorkspaceMember',
         )
+
+    image = models.ImageField(
+        upload_to='store/images',
+        null=True,
+        blank=True,
+        validators=[validate_file_size],
+    )
+    
+    class Meta:
+        abstract = True
+
 
     def __str__(self):
         return f'{self.name}'
