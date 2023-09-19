7440c91e1dbb3b99fa8ebb6d7f256a139f885a44
Reza Saraey <saraeygh@gmail.com>
2023-09-19 15:02:06 +0330

    fix: fix static_root

--- trello/settings.py
+++ trello/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Base url to serve media files
 MEDIA_URL = '/media/'
 
